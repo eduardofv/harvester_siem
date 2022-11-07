0b2b6f521c66d85ba3ab8f76ce6e4038585504d4
Eduardo Flores <eduardofv@gmail.com>
2022-11-07 12:49:59 -0600

    descargar catalogos

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,55 @@
 use reqwest::blocking::Client;
-use serde_json::{Value};
-//use std::collections::HashMap;
+use serde_json::{Map, Value};
+use std::fs;
 
+struct Catalog {
+    table_name: String,
+}
 
-fn main() -> Result<(), reqwest::Error> {
+fn load_catalog_definition() -> Map<String, Value> {
+    let text = fs::read_to_string("catalog_definition.json")
+        .expect("Could not read catalog_definition.json");
+    let catdef_json: Value = serde_json::from_str(&text)
+        .expect("Could not parse json for 'catalog_definition.json");
+    let catdef = catdef_json.as_object()
+        .expect("Error json.as_object").clone();
+    println!("{:?}", catdef);
+    catdef
+}
+
+fn get_catalog(client: &Client, cat_name: &str, url: &str) -> Value {
+    let activo = true.to_string();
+    let todos = true.to_string();
+    let response = client.get(url).send()
+        .expect(&format!("Could not get catalog {}", cat_name))
+        .text()
+        .expect(&format!("Could not get text from catalog {}", cat_name));
+
+    serde_json::from_str(&response)
+        .expect(&format!("Could not parse JSON {}", response))
+}
+
+fn main() {
+    let client = Client::new();
+
+    let catdef = load_catalog_definition();
+    //println!("{:?}", catdef);
+
+    for (cat_name, url) in catdef {
+        let url = url.as_str().unwrap();
+        let cat_name = cat_name.as_str();
+        println!("{}, {}", cat_name, url);
+        let cat = get_catalog(&client, cat_name, url);
+        println!("{:?}", cat);
+    }
+
+    /*******
+    let result = sandbox(client);
+    println!("{:?}", result);
+    */
+}
+
+fn sandbox(client: Client) -> Result<(), reqwest::Error> {
     //detalle establecimiento
     //let url = "https://siem.economia.gob.mx/detalle-establecimiento?id=1739";
 
@@ -12,7 +58,6 @@ fn main() -> Result<(), reqwest::Error> {
 
     //404
     //let url = "https://siem.economia.gob.mx/Mmunicipios-x-edo?idEntidadFederativa=1";
-    let client = Client::new();
 
     let response = client.get(url).send()?;
     println!("\nResponse: {:?}", response);
