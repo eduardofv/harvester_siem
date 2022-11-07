2fdbcd894629980df387c8bdc9b3c9454136a98d
Eduardo Flores <eduardofv@gmail.com>
2022-11-06 18:56:57 -0600

    Pruebas extraccion de jsons con serde

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 use reqwest::blocking::Client;
-use std::collections::HashMap;
+use serde_json::{Value};
+//use std::collections::HashMap;
 
 
 fn main() -> Result<(), reqwest::Error> {
+    //detalle establecimiento
+    //let url = "https://siem.economia.gob.mx/detalle-establecimiento?id=1739";
+
+    //catalogo estados
+    let url = "https://siem.economia.gob.mx/municipios-x-edo?idEntidadFederativa=1";
+
+    //404
+    //let url = "https://siem.economia.gob.mx/Mmunicipios-x-edo?idEntidadFederativa=1";
     let client = Client::new();
 
-    let response = client.get(
-        "https://httpbin.org"
-    ).send()?;
-    println!("{:?}",response);
+    let response = client.get(url).send()?;
+    println!("\nResponse: {:?}", response);
+
+    let text = response.text().unwrap();
+    println!("\nText: {:?}", text);
+
+    let obj: Value = serde_json::from_str(&text).expect("JSON not well formed");
+    println!("\nJSON Object: {:?}", obj);
+
+    let arr = obj.as_array().unwrap();
+    println!("\nArray?: {:?}", arr);
+
+    /*
+    let v: Vec<Value> = serde_json::from_str(&text).unwrap();
+    
+    for val in &v {
+        println!("{}", val);
+    }
+    */
+
+
     Ok(())
 }
